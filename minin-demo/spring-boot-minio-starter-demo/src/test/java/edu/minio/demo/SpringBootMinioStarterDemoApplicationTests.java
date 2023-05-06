46c20a29086a22086646f55c634e3f4ab93a5e19
toms <make_toms@hotmail.com>
2023-05-06 17:44:35 +0800

    toms:init...

--- minin-demo/spring-boot-minio-starter-demo/src/test/java/edu/minio/demo/SpringBootMinioStarterDemoApplicationTests.java
+++ minin-demo/spring-boot-minio-starter-demo/src/test/java/edu/minio/demo/SpringBootMinioStarterDemoApplicationTests.java
@@ -1,20 +1,18 @@
 package edu.minio.demo;
 
-import edu.minio.demo.test.MinioTemplateTest;
 import org.junit.jupiter.api.Test;
+import org.spring.minio.client.MinioTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootMinioStarterDemoApplicationTests {
-
     @Autowired
-    MinioTemplateTest minioTemplateTest;
+    private MinioTemplate minioTemplate;
 
     @Test
     void contextLoads() {
-        minioTemplateTest.sh();
+        minioTemplate.opsForBucket().createBucket("sadass");
     }
 
 }
